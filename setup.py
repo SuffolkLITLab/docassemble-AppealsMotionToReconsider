d620bd5a8a97c262592daac0774418e60f371a05
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-06-20 16:24:38 -0400

    Bump version, and force higher AL version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.AppealsMotionToReconsider',
-      version='1.0.4',
+      version='1.0.5',
       description=('Motion to reconsider template'),
       long_description='# docassemble.Motiontoreconsidertemplate\r\n\r\nMotion to reconsider template\r\n\r\n## Author\r\n\r\nRebecca (Ngan) Truong\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -54,7 +54,7 @@ setup(name='docassemble.AppealsMotionToReconsider',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.14.1', 'docassemble.MassAppealsCourt>=0.1.0'],
+      install_requires=['docassemble.AssemblyLine>=2.23.0', 'docassemble.MassAppealsCourt>=0.1.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AppealsMotionToReconsider/', package='docassemble.AppealsMotionToReconsider'),
      )
